f2e22a339b54f96019d12704dc9eefd229088700
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-04-05 14:13:35 +0100

    reset back to orginal error debugging method

--- securimage.php
+++ securimage.php
@@ -1,8 +1,6 @@
 <?php
 
-error_reporting(E_ALL);
-error_reporting(-1);
-ini_set('error_reporting', E_ALL);
+ error_reporting(E_ALL); ini_set('display_errors', 1); 
 
 /**
  * Project:     Securimage: A PHP class for creating and managing form CAPTCHA images<br />
