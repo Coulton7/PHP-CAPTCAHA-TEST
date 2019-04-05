6545d0917bc7ea22256b36de9809772b0e5c0a60
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-04-05 11:54:19 +0100

    reset back to orginal with slight path adjusment

--- securimage_show.php
+++ securimage_show.php
@@ -1,9 +1,10 @@
 <?php
+
 /**
  * Project:     Securimage: A PHP class for creating and managing form CAPTCHA images<br />
  * File:        securimage_show.php<br />
  *
- * Copyright (c) 2013, Drew Phillips
+ * Copyright (c) 2011, Drew Phillips
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
@@ -36,22 +37,27 @@
  * @link http://www.phpcaptcha.org Securimage PHP CAPTCHA
  * @link http://www.phpcaptcha.org/latest.zip Download Latest Version
  * @link http://www.phpcaptcha.org/Securimage_Docs/ Online Documentation
- * @copyright 2013 Drew Phillips
+ * @copyright 2012 Drew Phillips
  * @author Drew Phillips <drew@drew-phillips.com>
- * @version 3.6.6 (Nov 20 2017)
+ * @version 3.2RC2 (April 2012)
  * @package Securimage
  *
  */
+
 // Remove the "//" from the following line for debugging problems
-error_reporting(E_ALL); ini_set('display_errors', 1);
+// error_reporting(E_ALL); ini_set('display_errors', 1);
+
 require_once dirname(__FILE__) . '/securimage.php';
+
 $img = new Securimage();
+
 // You can customize the image by making changes below, some examples are included - remove the "//" to uncomment
+
 //$img->ttf_file        = './Quiff.ttf';
 //$img->captcha_type    = Securimage::SI_CAPTCHA_MATHEMATIC; // show a simple math problem instead of text
 //$img->case_sensitive  = true;                              // true to use case sensitve codes - not recommended
-//$img->image_height    = 90;                                // height in pixels of the image
-//$img->image_width     = $img->image_height * M_E;          // a good formula for image size based on the height
+//$img->image_height    = 90;                                // width in pixels of the image
+//$img->image_width     = $img->image_height * M_E;          // a good formula for image size
 //$img->perturbation    = .75;                               // 1.0 = high distortion, higher numbers = more distortion
 //$img->image_bg_color  = new Securimage_Color("#0099CC");   // image background color
 //$img->text_color      = new Securimage_Color("#EAEAEA");   // captcha text color
@@ -61,9 +67,11 @@ $img = new Securimage();
 //$img->signature_color = new Securimage_Color(rand(0, 64),
 //                                             rand(64, 128),
 //                                             rand(128, 255));  // random signature color
+
 // see securimage.php for more options that can be set
-// set namespace if supplied to script via HTTP GET
-if (!empty($_GET['namespace'])) $img->setNamespace($_GET['namespace']);
+
+
+
 $img->show();  // outputs the image and content headers to the browser
 // alternate use:
 $img->show( './backgrounds/code-bg.png' );
