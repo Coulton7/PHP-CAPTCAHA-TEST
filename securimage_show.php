1462b190079b18392bf1dbe43021275bcd47bbc1
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2019-04-05 13:42:39 +0100

    adding error reporting code to here also

--- securimage_show.php
+++ securimage_show.php
@@ -45,7 +45,9 @@
  */
 
 // Remove the "//" from the following line for debugging problems
- error_reporting(E_ALL); ini_set('display_errors', 1);
+error_reporting(E_ALL);
+error_reporting(-1);
+ini_set('error_reporting', E_ALL);
 
 require_once dirname(__FILE__) . '/securimage.php';
 
